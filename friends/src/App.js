81a808d918e547cdaa11ef56e402d4c23e632432
Khalil <khalilfoulks@gmail.com>
2020-07-21 16:51:42 -0400

    setup form

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
+import LoginForm from './components/LoginForm'
+import Login from './components/Login'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/login" component={Login}/> {" "}
+          <Route component={Login}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
