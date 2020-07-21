eeab78b83f37147cdd41a3a492ba1afdfb839f96
Khalil <khalilfoulks@gmail.com>
2020-07-21 18:45:10 -0400

    added friendcard to display list of friends

---
+++ friends/src/components/FriendCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function FriendCard(props){
+    const { friend } = props
+
+    if (!friend) {
+		return <h3>Working on fetching friends...</h3>;
+	}
+
+    return (
+            <div key ={friend.id} className={friend.name}>
+                <p>{friend.name}</p>
+                <p>{friend.age}</p>
+                <p>{friend.email}</p>
+            </div>
+    )
+}
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,8 +1,35 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+import FriendCard from './FriendCard'
 
 export default function Friends(){
+    const [friends, setFriends] = useState([])
+
+    const getData = () => {
+        axiosWithAuth()
+            .get("api/friends")
+            .then(res => {
+                console.log(res);
+                setFriends(res.data)
+            })
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+
     return (
-        <div>testing  Friends</div>
+        <div className="friends">
+            <div>testing  Friends</div>
+            {
+                friends.map(friend => {
+                        return (
+                            <FriendCard key={friend.id} friend={friend}/>
+                        )
+                })
+            }
+        </div>
+
     )
 }
